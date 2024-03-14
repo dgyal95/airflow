0c6980bf004d80e1a51cb131acb4382d347592eb
dgyal95 <dgyal95@gmail.com>
2024-03-14 13:07:01 +0900

    aa

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -18,7 +18,7 @@ with DAG(
 
     @task_group(group_id='first_group')
     def group_1():
-        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. '''
+        ''' task_group 데커레이터를 이용한 첫 번째 그룹입니다. ''' # docstring 함수 설명을 적어줌. airflow에서도 확인가능
 
         @task(task_id='inner_function1')
         def inner_func1(**kwargs):
@@ -33,6 +33,7 @@ with DAG(
         inner_func1() >> inner_function2
 
     with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다') as group_2:
+        # decorator말고 class 쓸때는 tooltip이 docstring과 같은 부분
         ''' 여기에 적은 docstring은 표시되지 않습니다'''
         @task(task_id='inner_function1')
         def inner_func1(**kwargs):
